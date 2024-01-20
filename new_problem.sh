10010353818928ac59ab111e8e2d6982f113207a
duval1024 <duval1024@gmail.com>
2024-01-20 12:07:47 +0800

    feat:新增题目

--- new_problem.sh
+++ new_problem.sh
@@ -1,8 +1,21 @@
 problem_name=$1
 
+# 定义sed -i 参数(数组)
+# Default case for Linux sed, just use "-i"
+sedi=(-i)
+case "$(uname)" in
+  # For macOS, use two parameters
+  Darwin*) sedi=(-i "")
+esac	
+
+########
+
 ## 新建题解页
 cp ./template/problem-template.md ./problem/$1.md
-sed -i "s/题目名字/$1/g" ./problem/$1.md
+
+## 修改标题
+#sed -i "s/题目名字/$1/g" $1.md
+sed "${sedi[@]}" "s/题目名字/$1/g" ./problem/$1.md
 
 ## 新建资源目录
 mkdir -p "./asset/$1"
